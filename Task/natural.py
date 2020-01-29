5579f0b3a88ed6a0d9db361c4044abe53f7e3367
revathy991 <60392459+revathy991@users.noreply.github.com>
2020-01-29 16:01:07 +0530

    Add files via upload

---
+++ Task/natural.py
@@ -0,0 +1,7 @@
+number = int(input("Please Enter any Number: "))
+total = 0
+
+for value in range(1, number + 1):
+    total = total + value
+
+print("The Sum of Natural Numbers from 1 to {0} =  {1}".format(number, total))
